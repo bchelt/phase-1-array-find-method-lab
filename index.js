9c917f0b9ac300ea658afe8eba9ce34a265c36c9
Brandon Helt <brandon.helt@yahoo.com>
2022-04-05 10:55:43 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(array) {
+    let element =  array.find((element) => element.result === "W" ? true : false);
+    if (element)
+        return element.year;
+}
